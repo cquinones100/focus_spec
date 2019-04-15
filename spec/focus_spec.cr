351bb5aa53ef2a099b26c7d6c15a5106ea38f8b4
Carlos Quinones <cquinones100@gmail.com>
2019-04-15 17:02:56 -0400

    adds it method

--- spec/focus_spec.cr
+++ spec/focus_spec.cr
@@ -3,16 +3,18 @@ require "../src/focus_spec"
 
 include FocusSpec
 
+SHOULD_RUN_CONTEXT = " should run context\n"
+SHOULD_RUN_DESCRIBE = " should run describe\n"
+SHOULD_RUN_IT = " should run it\n"
+SHOULD_NOT_RUN_CONTEXT = " should not run context\n"
+SHOULD_NOT_RUN_DESCRIBE = " should not run describe\n"
+SHOULD_NOT_RUN_IT = " should not run it\n"
+
 describe "fcontext" do
   context "when focussed" do
     fcontext "something" do 
       it "runs" do
-        true.should eq true
-      end
-    end
-
-    fcontext "something" do
-      it "runs" do
+        print SHOULD_NOT_RUN_CONTEXT
         true.should eq true
       end
     end
@@ -21,25 +23,45 @@ describe "fcontext" do
   context "when not focussed" do
     context "something else" do
       it "doesn't run" do
+        print SHOULD_NOT_RUN_CONTEXT
         false.should eq true
       end
     end
   end
 end
 
-pending "fit" do
-  context "when focussed" do
+fdescribe "fdescribe" do
+  it "runs" do
+    print SHOULD_NOT_RUN_DESCRIBE
+
+    true.should eq true
+  end
+end
+
+describe "describe" do
+  it "doesn't run" do
+    print SHOULD_NOT_RUN_DESCRIBE
+    
+    false.should eq false
+  end
+end
+
+fdescribe "fit" do
+  fcontext "when focussed" do
     fit "runs" do
+      print " running fit "
       true.should eq true
     end
 
-    fit "runs" do
-      true.should eq true
+    it "runs" do
+      print " should not run it "
+      false.should eq true
     end
   end
 
   context "something else" do
     it "doesn't run" do
+      print " should not run it "
       false.should eq true
     end
   end
--- src/focus_spec.cr
+++ src/focus_spec.cr
@@ -15,8 +15,20 @@ module FocusSpec
     FocusSpec.focussed_contexts_stack.push describe
   end
 
+  def Spec::RootContext.fit(
+    description = "assert",
+    file = __FILE__,
+    line = __LINE__,
+    end_line = __END_LINE__,
+    &block
+  )
+    execute_it(description.to_s, file, line, end_line, &block)
+  end
+
   def Spec::RootContext.describe(description, file, line, &block)
-    execute_describe(description, file, line, &block) if FocusSpec.focussed_contexts_stack.size.zero?
+    if FocusSpec.focussed_contexts_stack.size.zero?
+      execute_describe(description, file, line, &block) 
+    end
   end
 
   def Spec::RootContext.execute_describe(description, file, line, &block)
@@ -28,7 +40,50 @@ module FocusSpec
     @@contexts_stack.pop
   end
 
+  def Spec::RootContext.execute_it(description = "assert", file = __FILE__, line = __LINE__, end_line = __END_LINE__, &block)
+    description = description.to_s
+
+    Spec::RootContext.check_nesting_spec(file, line) do
+      return unless Spec.matches?(description, file, line, end_line)
+
+      Spec.formatters.each(&.before_example(description))
+
+      start = Time.monotonic
+      begin
+        Spec.run_before_each_hooks
+        block.call
+        Spec::RootContext.report(:success, description, file, line, Time.monotonic - start)
+      rescue ex : Spec::AssertionFailed
+        Spec::RootContext.report(:fail, description, file, line, Time.monotonic - start, ex)
+        Spec.abort! if Spec.fail_fast?
+      rescue ex
+        Spec::RootContext.report(:error, description, file, line, Time.monotonic - start, ex)
+        Spec.abort! if Spec.fail_fast?
+      ensure
+        Spec.run_after_each_hooks
+
+        # We do this to give a chance for signals (like CTRL+C) to
+        # be handled,
+        # which currently are only handled when there's a fiber
+        # switch
+        # (IO stuff, sleep, etc.). Without it the user might wait
+        # more than needed
+        # after pressing CTRL+C to quit the tests.
+        Fiber.yield
+      end
+    end
+  end
+
   def fcontext(description, file = __FILE__, line = __LINE__, &block)
     Spec::RootContext.fdescribe(description.to_s, file, line, &block)
   end
+
+  def fdescribe(description, file = __FILE__, line = __LINE__, &block)
+    Spec::RootContext.fdescribe(description.to_s, file, line, &block)
+  end
+
+  def fit(description = "assert", file = __FILE__, line = __LINE__, end_line = __END_LINE__, &block)
+    Spec::RootContext.fit(description.to_s, file, line, end_line, &block)
+  end
 end
+
